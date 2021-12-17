5cc8bdf3770a79ebd94a3f9fd143fa0093c02481
Nikhil Anand <nkhlanand@gmail.com>
2021-12-17 18:32:48 -0500

    Made training loop into an ABC

---
+++ base/trainloop.py
@@ -0,0 +1,21 @@
+import abc
+
+class TrainLoop(abc.ABC):
+	"""
+	An instance of a training loop. It is useful to make this
+	an abstract base class because depending on the metrics we want
+	to log, the training loop will look very different. It is
+	therefore useful to have a blueprint and use a different
+	instance for different metrics (otherwise the loop will
+	become way too long/complicated.)
+	"""
+
+	@abc.abstractmethod
+	def description(self):
+		"""A description of this trianing loop."""
+		pass
+
+	@abc.abstractmethod
+	def loop(self):
+		"""The loop itself."""
+		pass
