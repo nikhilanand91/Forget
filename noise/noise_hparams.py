477e0bd15af456b372636a9b4fc4dbcdcf35985f
Nikhil Anand <nkhlanand@gmail.com>
2022-01-17 19:08:52 -0500

    Update noise_hparams.py

--- noise/noise_hparams.py
+++ noise/noise_hparams.py
@@ -5,18 +5,18 @@ from base.hparams import HParams
 class NoiseHParams(HParams):
     """This class contains the training hyperparameter details."""
 
-    allowed_params = {'--model_directory': str, '--iteration': int, 'replicates': int,
+    allowed_params = {'--model_directory': str, '--iteration': int, '--replicates': int,
                       '--noise_min': int, '--noise_max': int, '--num_samples': int}
     param_desc = {'--model_directory': 'directory where trained models are located',
+                  '--iteration': 'the iteration of model to add noise to',
                   '--replicates:': 'which replicates to do noise experiments on (e.g, 4 means replicates 0, 1, ... , 4)',
-                  '--iter': 'the iteration of model to add noise to',
                   '--noise_min': 'minimum level of noise (default 0)',
                   '--noise_max': 'maximum level of noise (default 1)',
                   '--num_samples': 'number of noise samples (default 100)'}
 
-    model_directory: str
-    iteration: int
-    replicates: int
+    model_directory: str = ''
+    iteration: int = 4000
+    replicates: int = 1
     noise_min: int = 0
     noise_max: int = 1
     num_samples: int = 100
